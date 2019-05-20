e7000ca26d0ae17e272ced794b391c19685896c9
saratims123 <saratimsmichaelsen@gmail.com>
2019-05-20 14:54:19 +0200

    Fuck dig din lille stramme luder

---
+++ speechCode.js
@@ -0,0 +1,85 @@
+let words = [];
+let sentence = "";
+let resultP;
+let leftDiv;
+let counter;
+let cnv, myRec, btn, txt, img;
+
+function setup() {
+    let SpeechRecognition = window.webkitSpeechRecognition ||
+        window.mozSpeechRecognition ||
+        window.msSpeechRecognition ||
+        window.oSpeechRecognition ||
+        window.SpeechRecognition;
+
+    cnv = createCanvas(400, 400);
+    background('white');
+    txt = createElement("h5", "Say something..")
+        .position(40, 200)
+        .style("color:black;")
+        .hide();
+
+    resultP = createP("")
+        .position(40, 220)
+        .parent(txt);
+    //Check browser comp
+    if (SpeechRecognition !== undefined) {
+        btn = createButton("Klik for at aktivere mikrofon")
+            .position(40, 200)
+            .style("font-size:1em;background-color:#33C3F0;border-color:#33C3F0;border-radius:8px;color:white;cursor:pointer;")
+            .mousePressed(function () {
+                btn.hide();
+                txt.show();
+                myRec = new p5.SpeechRec();
+                myRec.continuous = true;
+                myRec.interimResults = true;
+                myRec.onResult = showResult;
+                myRec.start();
+            });
+    }
+}
+
+function draw() {}
+
+function showResult() {
+    if (myRec.resultValue == true) {
+        sentence = myRec.resultString.split(' ').pop();
+        resultP.html(sentence);
+        if(sentence.includes("Sara")){
+            switchImage('assets/Sara.jpg');
+            }
+        if(sentence.includes("søster")){
+            switchImage('images/søster.jpg');
+            }    
+        if(sentence.includes("fodbold")){
+            switchImage('assets/fodbold.jpg');
+            }
+        
+        if(sentence.includes("basket")){
+        switchImage('assets/basket.jpg');
+        }
+
+        if(sentence.includes("brundhåret")){
+            switchImage('images/brunhåret.png');
+            }
+            
+            if(sentence.includes("blondt")){
+            switchImage('images/blondt.png');
+            }
+        
+            
+            
+        
+    }   
+}
+
+
+function switchImage(url){
+    if(img == undefined){
+        img = createImg(url)
+        .position(width/2, height/2)
+        .style("width:500px;height:500px");
+    }else{
+        img.attribute('src', url)
+    }
+}
